a39f5e3e2744813c7e7142337c870ddafdc57774
ThizThizzyDizzy <thizthizzydizzy@gmail.com>
2025-03-28 03:51:48 -0400

    Added multipliers for iron and gold, bumped version to 1.1.0

--- src/com/thizthizzydizzy/propernuggetsmelting/FurnaceSmelt.java
+++ src/com/thizthizzydizzy/propernuggetsmelting/FurnaceSmelt.java
@@ -1,6 +1,5 @@
 package com.thizthizzydizzy.propernuggetsmelting;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.FurnaceSmeltEvent;
@@ -17,7 +16,6 @@ public class FurnaceSmelt implements Listener{
             if(event.getResult().getAmount()>1)return;
             ItemStack tool = event.getSource();
             short max = tool.getType().getMaxDurability();
-            float percent = (max-tool.getDurability())/(float)max;
             int nuggets = 0;
             switch(tool.getType()){
                 case IRON_AXE:
@@ -25,19 +23,16 @@ public class FurnaceSmelt implements Listener{
                     nuggets = 9*3;
                     break;
                 case CHAINMAIL_BOOTS:
-                    percent*=plugin.getConfig().getDouble("chainmail-multiplier");
                 case IRON_BOOTS:
                 case GOLDEN_BOOTS:
                     nuggets = 9*4;
                     break;
                 case CHAINMAIL_CHESTPLATE:
-                    percent*=plugin.getConfig().getDouble("chainmail-multiplier");
                 case IRON_CHESTPLATE:
                 case GOLDEN_CHESTPLATE:
                     nuggets = 9*8;
                     break;
                 case CHAINMAIL_HELMET:
-                    percent*=plugin.getConfig().getDouble("chainmail-multiplier");
                 case IRON_HELMET:
                 case GOLDEN_HELMET:
                     nuggets = 9*5;
@@ -45,14 +40,12 @@ public class FurnaceSmelt implements Listener{
                 case IRON_HORSE_ARMOR:
                 case GOLDEN_HORSE_ARMOR:
                     nuggets = 9*6;
-                    percent = 1;
                     break;
                 case IRON_HOE:
                 case GOLDEN_HOE:
                     nuggets = 9*2;
                     break;
                 case CHAINMAIL_LEGGINGS:
-                    percent*=plugin.getConfig().getDouble("chainmail-multiplier");
                 case IRON_LEGGINGS:
                 case GOLDEN_LEGGINGS:
                     nuggets = 9*7;
@@ -72,8 +65,46 @@ public class FurnaceSmelt implements Listener{
                 default:
                     return;
             }
-            nuggets*=percent;
-            event.getResult().setAmount(Math.max(1,nuggets));
+            float percent = (max-tool.getDurability())/(float)max;
+            if(tool.getType()==Material.IRON_HORSE_ARMOR||tool.getType()==Material.GOLDEN_HORSE_ARMOR){
+                percent = 1;
+            }
+            switch(tool.getType()){
+                case CHAINMAIL_BOOTS:
+                case CHAINMAIL_CHESTPLATE:
+                case CHAINMAIL_HELMET:
+                case CHAINMAIL_LEGGINGS:
+                    percent *= plugin.getConfig().getDouble("chainmail-multiplier");
+                    break;
+                case IRON_AXE:
+                case IRON_BOOTS:
+                case IRON_CHESTPLATE:
+                case IRON_HELMET:
+                case IRON_HOE:
+                case IRON_HORSE_ARMOR:
+                case IRON_LEGGINGS:
+                case IRON_PICKAXE:
+                case IRON_SHOVEL:
+                case IRON_SWORD:
+                    percent *= plugin.getConfig().getDouble("iron-multiplier");
+                    break;
+                case GOLDEN_AXE:
+                case GOLDEN_BOOTS:
+                case GOLDEN_CHESTPLATE:
+                case GOLDEN_HELMET:
+                case GOLDEN_HOE:
+                case GOLDEN_HORSE_ARMOR:
+                case GOLDEN_LEGGINGS:
+                case GOLDEN_PICKAXE:
+                case GOLDEN_SHOVEL:
+                case GOLDEN_SWORD:
+                    percent *= plugin.getConfig().getDouble("gold-multiplier");
+                    break;
+                default:
+                    return;
+            }
+            nuggets *= percent;
+            event.getResult().setAmount(Math.max(1, nuggets));
         }
     }
 }
